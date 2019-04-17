2f8c14bbb87143d84ffb7481e5362105f0e97b56
bbogatov <bogatov.bogdan@gmail.com>
2019-04-17 12:25:46 +0300

    Add button that removes itself when clicked

--- app/src/main/java/com/example/helloworldbutton/MainActivity.java
+++ app/src/main/java/com/example/helloworldbutton/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.helloworldbutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button helloWorldButton = (Button) findViewById(R.id.button);
+
+        helloWorldButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ViewGroup viewGroup = (ViewGroup) helloWorldButton.getParent();
+                viewGroup.removeView(helloWorldButton);
+            }
+        });
+
     }
 }
